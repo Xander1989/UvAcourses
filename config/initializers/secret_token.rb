3fd0c38569e21ad70c61866784eb51c1740985c1
Mark van Gerwen <mark.vangerwen@gmail.com>
2013-12-04 13:34:38 +0100

    Eerste Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UvAcourses::Application.config.secret_token = 'b08535ab7557a45d1f15c28e60ed6d7a3931ea41a00b5e420590068110c583efccd422b07b1ae5e6a508b9aa4c3fb624e5dc7447fb522f9643858a22a5d67479'
