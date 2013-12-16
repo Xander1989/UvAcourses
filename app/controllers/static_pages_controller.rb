b71a597d00b6fb972037042c52fd94fe88669088
Mark van Gerwen <mark.vangerwen@gmail.com>
2013-12-16 22:18:25 +0100

    design doc

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,15 +5,10 @@ class StaticPagesController < ApplicationController
   def help
   end
 
-<<<<<<< HEAD
   def contact
   end
 
   def about
   end
-  
-=======
-  def profile
-  end
->>>>>>> 80fb8d210bbe102f1d64d9d62ce69277a2a2fd01
+
 end
