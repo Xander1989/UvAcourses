397b3a1404022ae264f2b332a3cad8e93c99380a
Mark van Gerwen <mark.vangerwen@gmail.com>
2013-12-17 11:42:58 +0100

    test 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 UvAcourses::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/profile"
+  root 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/profile', to: 'static_pages#profile', via: 'get'
+
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
