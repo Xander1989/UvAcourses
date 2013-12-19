fc86428ef83b982b77d5abb96da6b0a0362edd66
Mark van Gerwen <mark.vangerwen@gmail.com>
2013-12-19 16:54:48 +0100

    Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'nokogiri'
+require 'open-uri'
+require 'rubygems'
+require 'sqlite3'
+courses = Nokogiri::XML(File.open("courses.xml"))
+courses.css('course').each do |node|
+  children = node.children
+
+  Courses.create(
+    acode: node['acode'],
+        code: node['code'],
+        sgid: node['sgid'],
+    name: children.css('name').inner_text,
+    ects: children.css('ects').inner_text,
+    year: children.css('year').inner_text,
+        period: children.css('period').inner_text,
+        institute: children.css('institute').inner_text,
+        description: children.css('description').inner_text,
+        timetable: children.css('timetable').inner_text,
+        lecture_material: children.css('lecture_material').inner_text,
+        goals: children.css('goals').inner_text,
+        participant_count: children.css('participant_count').inner_text,
+        specialities: children.css('specialities').inner_text,
+        examination: children.css('examination').inner_text
+  )
+  
+end
