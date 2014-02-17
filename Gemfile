1535f9b9676f7334115510ba790ba4965d621cdb
sergelichtveld <slichtveld@icloud.com>
2014-02-17 11:56:35 +0100

    Updated HTML to fix sidebar bug

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '1.9.3'
+ruby '2.0.0'
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.0.2'
@@ -32,7 +32,6 @@ group :doc do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
   
   
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchesController < ApplicationController
 
-  def show
-    @search = Search.find(params[:id])
-    @courses = @search.courses
-    end   
+  	def show
+    	@search = Search.find(params[:id])
+    	@courses = @search.courses
+	end   
 
 end
